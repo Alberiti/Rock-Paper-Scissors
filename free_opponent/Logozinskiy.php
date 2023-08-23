a5cefceaac5d4d45d595da2215689cac24370a72
Albert <101431728+Alberiti@users.noreply.github.com>
2023-08-23 14:26:31 +0300

    Create Logozinskiy.php

---
+++ free_opponent/Logozinskiy.php
@@ -0,0 +1,14 @@
+<?php
+function logozinskiy($inputNumber) {
+    if ($inputNumber === 1){
+        return 3;
+    }
+    else if ($inputNumber === 2 ){
+        return 1;
+    }
+    else if($inputNumber === 3){
+        return 2;
+    }
+}
+
+?>
